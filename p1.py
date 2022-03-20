d43eb3157b58af8e7756b3882170fe2765d768f3
jayasree <jayatutta0903@gmail.com>
2022-03-20 21:25:17 +0530

    these are my python files

---
+++ p1.py
@@ -0,0 +1,60 @@
+from numpy import  *
+'''import array
+ar=array.array("i",[10,20,30,40])
+print(ar)'''
+
+'''import array as arr
+ar=arr.array("i",[10,20,30,40])
+print(ar)'''
+
+'''from array import *
+a=array("i",[10,20,30,40])
+print(a)'''
+
+'''import numpy
+a=numpy.array([1,2,3,4])
+print(a)'''
+
+'''import numpy as np
+a=np.array([1,2,3,4])
+print(a)'''
+
+'''from numpy import  *
+a=array([1,2,3,4]) # one dimensional array
+print(a)'''
+
+
+
+'''from numpy import  *
+a=array([[1,2,3,4],[1,2,3,4]]) # two dimensional array
+print(a)'''
+
+'''from numpy import  *
+a=array([[[1,2,3,4],[1,2,3,4]],[[1,2,3,4],[1,2,3,4]]]) # three dimensional array
+print(a)'''
+
+'''from numpy import  *
+a=array([1,2,3,4]) 
+print(a.shape)
+
+from numpy import  *
+a=array([[1,2,3,4],[1,2,3,4],[1,2,3,4]])
+print(a.shape)
+
+from numpy import  *
+a=array([[[1,2,3,4],[1,2,3,4]],[[1,2,3,4],[1,2,3,4]],[[1,2,3,4],[1,2,3,4]]]) # three dimensional array
+print(a.shape)'''
+
+a=array([1.1,2,3,4,5,6,7,8])
+print(a.dtype)
+a=array([[1,2,3,4],[1,2,3,4],[1,2,3,4]])
+a=array([[[1,2,3,4],[1,2,3,4]],[[1,2,3,4],[1,2,3,4]],[[1,2,3,4],[1,2,3,4]]])
+print(a.ndim)
+
+
+
+
+
+
+
+
